ba79b3467578cb79ccaaede2fa976570ddef8110
Odagawa Takahiro <odagawa.takahiro.57w@st.kyoto-u.ac.jp>
2020-11-30 23:16:12 +0900

    Emulsuion Wrapper project started

---
+++ src/EmulsionWrapper/EmulsionWrapper.cpp
@@ -0,0 +1,16 @@
+// system includes
+#include <string>
+
+// boost includes
+#include <boost/log/trivial.hpp>
+
+// B2 includes
+#include "B2Reader.hh"
+#include "B2HitSummary.hh"
+#include "B2BeamSummary.hh"
+
+int main(int argc, char *argv[]) {
+
+  std::exit(0);
+  
+}
