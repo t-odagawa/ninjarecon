a680a71e75a4f1c2dd9c38820ba94c897ea8eaf3
Odagawa Takahiro <odagawa.takahiro.57w@st.kyoto-u.ac.jp>
2020-12-04 21:57:37 +0900

    library project started

---
+++ lib/NTBMSummary.hh
@@ -0,0 +1,27 @@
+#ifndef NTBMSUMMARY_HH
+#define NTBMSUMMARY_HH
+
+#include <TObject.h>
+
+#ifdef __ROOTCLING__
+#pragma link off globals;
+#pragma link off classes;
+#pragma link off functions;
+#pragma link C++ class NTBMSummary+;
+#endif
+
+/**
+ * Class containing info about scintillator detectors for (muon) track matching
+ * between Scintillation Tracker/BabyMIND and TSS/Emulsion Shifter/ECC
+ */
+
+class NTBMSummary: public TObject {
+
+public :
+
+private :
+  
+  
+}
+
+#endif
