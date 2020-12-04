cd58c205a1859b36f60aeed745505ecec701792a
Odagawa Takahiro <odagawa.takahiro.57w@st.kyoto-u.ac.jp>
2020-12-04 21:20:13 +0900

    Track match project started

--- src/TrackMatch/TrackMatch.cpp
+++ src/TrackMatch/TrackMatch.cpp
@@ -2,15 +2,53 @@
 #include <string>
 
 // booost includes
+#include <boost/log/core.hpp>
 #include <boost/log/trivial.hpp>
+#include <boost/log/expressions.hpp>
+
+// root includes
 
 // B2 includes
 #include "B2Reader.hh"
 #include "B2Writer.hh"
 #include "B2HitSummary.hh"
 
+namespace logging = boost::log;
+
 int main(int argc, char *argv[]) {
 
+  logging::core::get()->set_filter
+    (
+     logging::trivial::severity >= logging::trivial::info
+     );
+
+  BOOST_LOG_TRIVIAL(info) << "==========NINJA Track Matching Start==========";
+
+  if (argc != 2) {
+    BOOST_LOG_TRIVIAL(error) << "Usage : " << argv[0]
+			     << " <input B2 file path> <output B2 file path>";
+    std::exit(1);
+  }
+
+  try {
+    B2Reader reader(argv[1]);
+    B2Writer writer(argv[2]);
+
+    while (reader.ReadNextSpill() > 0) {
+      auto &input_spill_summary = reader.GetSpillSummary();
+
+
+      
+    }
+    
+  } catch (const std::runtime_error &error) {
+    BOOST_LOG_TRIVIAL(fatal) << "Runtime error : " << error.what();
+  } catch (const std::invalid_argument &error) {
+    BOOST_LOG_TRIVIAL(fatal) << "Invalid argument error : " << error.what();
+    std::exit(1);
+  }
+  
+  BOOST_LOG_TRIVIAL(info) << "==========NINJA Track Matching Finish==========";
   std::exit(0);
 
 }
