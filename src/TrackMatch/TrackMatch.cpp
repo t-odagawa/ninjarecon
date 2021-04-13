ff1ba430fa155614b10a3df09395f9622a08f5eb
ODAGAWA Takahiro <odagawa.takahiro.57w@st.kyoto-u.ac.jp>
2021-04-13 21:37:28 +0900

    revert frot debug to info

--- src/TrackMatch/TrackMatch.cpp
+++ src/TrackMatch/TrackMatch.cpp
@@ -777,8 +777,8 @@ int main(int argc, char *argv[]) {
 
   logging::core::get()->set_filter
     (
-     //logging::trivial::severity >= logging::trivial::info
-     logging::trivial::severity >= logging::trivial::debug
+     logging::trivial::severity >= logging::trivial::info
+     //logging::trivial::severity >= logging::trivial::debug
      );
 
   BOOST_LOG_TRIVIAL(info) << "==========NINJA Track Matching Start==========";
